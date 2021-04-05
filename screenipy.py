bf74c6c92af54974fb117a0aabd9bba635dea277
Pranjal Joshi <joshi.pranjal5@gmail.com>
2021-04-05 15:59:44 +0530

    Initial commit - MA, Volume, Consolidation scanning added

---
+++ screenipy.py
@@ -0,0 +1,244 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import urllib
+import requests
+import yfinance as yf
+from nsetools import Nse
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tabulate import tabulate
+
+# Constants
+DEBUG = False
+consolidationPercentage = 4
+volumeRatio = 2.5
+minLTP = 25.0
+period = '1y'
+duration = '1d'
+TEST_STKCODE = "HAPPSTMNDS"
+
+nse = Nse()
+
+# Decoration Class
+class colorText:
+	HEAD = '\033[95m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	WARN = '\033[93m'
+	FAIL = '\033[91m'
+	END = '\033[0m'
+	BOLD = '\033[1m'
+	UNDR = '\033[4m'
+
+# Global Variabls
+screenResults = pd.DataFrame(columns=['Stock','Consolidating','Breaking-Out','MA-Signal','Volume'])
+screeningDictionary = {
+    'Stock': "",
+    'Consolidating': "",
+    'Breaking-Out': "",
+    'MA-Signal': "",
+    'Volume': ""
+}
+listStockCodes = []
+
+# Get system wide proxy for networking
+proxyServer = urllib.request.getproxies()['http']
+
+# Fetch all stock codes from NSE
+def fetchStockCodes():
+    global listStockCodes
+    print("Getting Stock Codes From NSE... ",end='')
+    listStockCodes = list(nse.get_stock_codes())[1:]
+    if len(listStockCodes) > 10:
+        print("=> Done! Fetched %d stock codes." % len(listStockCodes))
+    else:
+        print("=> Error getting stock codes from NSE!")
+        sys.exit("Exiting script..")
+
+# Fetch stock price data from Yahoo finance
+def fetchStockData(stockCode):
+    data = yf.download(
+        tickers = stockCode+".NS",
+        period = period,
+        duration = duration,
+        proxy = proxyServer
+    )
+    print("Fetching prices of %s..." % stockCode, end='')
+    
+    if len(data) == 0:
+        print("=> Failed to fetch!")
+        return None
+    print("=> Done!")
+    return data
+
+# Preprocess the acquired data
+def preprocessData(data, daysToLookback=30):
+    sma = data.rolling(window=50).mean()
+    lma = data.rolling(window=200).mean()
+    vol = data.rolling(window=20).mean()
+    data.insert(6,'SMA',sma['Close'])
+    data.insert(7,'LMA',lma['Close'])
+    data.insert(8,'VolMA',vol['Volume'])
+    data = data[::-1]               # Reverse the dataframe
+    data = data[1:]                 # Eliminate Headers
+    data = data.head(daysToLookback)
+    data = data.replace(np.nan, 0)
+    if DEBUG:
+        print(data)
+    return data
+
+# Analyze stock dataframe to determine possibility of box breakout
+def findBoxBreakout(data):
+    highest = []
+    openclose = []
+    for i in range(0,len(data)):
+        highest.append(data.iloc[i]['High'])
+        if data.iloc[i]['Close'] > data.iloc[i]['Open']:      # Green Candle
+            openclose.append(data.iloc[i]['Close'])
+        else:
+            openclose.append(data.iloc[i]['Open'])            # Red Candle
+    return (highest, openclose)
+
+# Linear regression
+def showBoxfitLine(data,daysToLookback=30, stockCode=''):
+    x = np.arange(daysToLookback+1,1,-1)
+    y = np.array(data)
+    m,c = np.polyfit(x,y,1)
+    print("Slope = %f\tIntercept = %f\tLine of angle with X-axis = %f" % (round(m,2),round(c,2),np.rad2deg(np.arctan(m))))
+    plt.plot(x,y,'o')
+    plt.plot(x,m*x+c)
+    plt.xlabel("Days")
+    plt.ylabel("Price")
+    plt.title(stockCode)
+    plt.show()
+
+# Simple Average
+def showBoxfitLineAverage(data,daysToLookback=30, stockCode=''):
+    x = np.arange(daysToLookback+1,1,-1)
+    y = np.array(data)
+    z = np.ones(daysToLookback) * y.mean()
+    print("Intercept = %f" % z[0])
+    plt.plot(x,y,'o')
+    plt.plot(x,z)
+    plt.xlabel("Days")
+    plt.ylabel("Price")
+    plt.title(stockCode)
+    plt.show()
+
+# Just a random function to plot averaged line
+def r(daysToLookback=30):
+    x = np.arange(1,daysToLookback+1)
+    y = np.random.rand(daysToLookback)
+    z = np.ones(daysToLookback) * y.mean()
+    plt.plot(x,y,'o')
+    plt.plot(x,z)
+    plt.xlabel("Days")
+    plt.ylabel("Price")
+    plt.title("Randomized Price Action")
+    plt.show()
+
+# Validate LTP than minimum threshold
+def validateLTP(data,minLTP=minLTP):
+    if(data.describe()['Close']['mean'] >= minLTP):
+        if DEBUG:
+            print("LTP >= %d => Valid!" % minLTP)
+        return True
+    else:
+        if DEBUG:
+            print("LTP <= %d => Invalid!" % minLTP)
+        return False
+
+# Validate if share is in valid box
+def validateBox(data,boxHeight=20):
+    print("Validating Box conditions... ")
+    hs = data.describe()['High']['max']
+    hc = data.describe()['Close']['max']
+    lc = data.describe()['Close']['min']
+    ls = data.describe()['Low']['min']
+    atp = data.describe()['Close']['min']
+    #max = atp + atp*(boxHeight/200)
+    #min = atp - atp*(boxHeight/200)
+    max = hs
+    min = ls
+    if DEBUG :
+        print("Max = %f\tMin = %f" % (max,min))
+        print("HC = %f\tLC = %f\tHS = %f\tLS = %f" % (hc, lc, hs, ls))
+    if(hc <= max and lc >= min):
+        print("Box => Valid!")
+        return True
+    print("Box => Invalid!")
+    return False
+
+# Validate if share prices are consolidating
+def validateConsolidation(data, dict, percentage=2.5):
+    print("Validating price consolidation... ")
+    hc = data.describe()['Close']['max']
+    lc = data.describe()['Close']['min']
+    if ((hc - lc) <= (hc*percentage/100)):
+        dict['Consolidating'] = colorText.BOLD + colorText.GREEN + "Range = " + str(round((abs((hc-lc)/hc)*100),2))+"%" + colorText.END
+        if DEBUG:
+            print("Consolidation => Valid!")
+    else:
+        if DEBUG:
+            print("Consolidation => Invalid!")
+        dict['Consolidating'] = colorText.FAIL + "Range = " + str(round((abs((hc-lc)/hc)*100),2)) + "%" + colorText.END
+
+# Validate Moving averages
+def validateMovingAverages(data, dict):
+    recent = data.head(1)
+    if(recent['SMA'][0] > recent['LMA'][0]):
+        dict['MA-Signal'] = colorText.BOLD + colorText.GREEN + 'Bullish' + colorText.END
+    elif(recent['SMA'][0] < recent['LMA'][0]):
+        dict['MA-Signal'] = colorText.BOLD + colorText.FAIL + 'Bearish' + colorText.END
+    else:
+        dict['MA-Signal'] = colorText.BOLD + colorText.WARN + 'Neutral' + colorText.END
+
+# Validate if volume of last day is higher than avg
+def validateVolume(data, dict, volumeRatio=2.5):
+    recent = data.head(1)
+    ratio = recent['Volume'][0]/recent['VolMA'][0]
+    if(ratio >= volumeRatio):
+        dict['Volume'] = colorText.BOLD + colorText.GREEN + str(round(ratio,2)) + "x" + colorText.END
+    else:
+        dict['Volume'] = colorText.BOLD + colorText.FAIL + str(round(ratio,2)) + "x" + colorText.END
+
+if __name__ == "__main__":
+    os.system("clear")
+    fetchStockCodes()
+    for stock in listStockCodes:
+        try:
+            data = fetchStockData(stock)
+            processedData = preprocessData(data, daysToLookback=14)
+            screeningDictionary['Stock'] = colorText.BOLD + colorText.BLUE + stock + colorText.END
+            validateConsolidation(processedData, screeningDictionary, percentage=consolidationPercentage)
+            validateMovingAverages(processedData, screeningDictionary)
+            validateVolume(processedData, screeningDictionary, volumeRatio=volumeRatio)
+            highest, openclose = findBoxBreakout(data)
+            #validateLTP(processedData)
+            #validateBox(processedData)
+            screenResults = screenResults.append(screeningDictionary,ignore_index=True)
+        except KeyboardInterrupt:
+            print(colorText.BOLD + colorText.FAIL + "[+] Script terminated by the user." + colorText.END)
+            print(tabulate(screenResults, headers='keys', tablefmt='psql'))
+            sys.exit(0)
+        except Exception as e:
+            print(colorText.FAIL + "[+] Exception Occured while Screening! Moving on.." + colorText.END)
+            print(e)
+    #showBoxfitLine(highest, daysToLookback=20, stockCode=TEST_STKCODE)
+    #showBoxfitLineAverage(highest, daysToLookback=20, stockCode=TEST_STKCODE)
+    #showBoxfitLine(openclose, daysToLookback=20, stockCode=TEST_STKCODE)
+
+    '''
+    plt.subplot(211)
+    #plt.hist(highest,bins=np.arange(round(min(highest)),round(max(highest)),1))
+    plt.hist(highest,bins='auto')
+    plt.title("Occurrence of Upper Shadows near the Price")
+    plt.subplot(212)
+    #plt.hist(openclose,bins=np.arange(round(min(openclose)),round(max(openclose)),1))
+    plt.hist(openclose,bins='auto')
+    plt.title("Occurrence of Open Close near the Price")
+    plt.show()
+    '''
