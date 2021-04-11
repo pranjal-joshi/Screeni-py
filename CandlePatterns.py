f70563268dbcbdc09c87629e3cdac2012d6c8cea
Pranjal Joshi <joshi.pranjal5@gmail.com>
2021-04-11 21:03:00 +0530

    Patterns priority updated

--- CandlePatterns.py
+++ CandlePatterns.py
@@ -18,16 +18,6 @@ class CandlePatterns:
     def findPattern(self, data, dict, saveDict):
         data = data.head(4)
         data = data[::-1]
-        
-        check = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
-                dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Bullish Engulfing' + colorText.END
-                saveDict['Pattern'] = 'Bullish Engulfing'
-            else:
-                dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Bearish Engulfing' + colorText.END
-                saveDict['Pattern'] = 'Bearish Engulfing'
-            return True
 
         check = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
         if(check.tail(1).item() != 0):
@@ -40,17 +30,20 @@ class CandlePatterns:
             dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Evening Star' + colorText.END
             saveDict['Pattern'] = 'Evening Star'
             return True
-        
-        check = talib.CDLHAMMER(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Hammer' + colorText.END
-            saveDict['Pattern'] = 'Hammer'
-            return True
 
-        check = talib.CDLINVERTEDHAMMER(data['Open'], data['High'], data['Low'], data['Close'])
+        check = talib.CDL3LINESTRIKE(data['Open'], data['High'], data['Low'], data['Close'])
         if(check.tail(1).item() != 0):
-            dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Inverted Hammer' + colorText.END
-            saveDict['Pattern'] = 'Inverted Hammer'
+            if(check.tail(1).item() > 0):
+                dict['Pattern'] = colorText.BOLD + colorText.GREEN + '3 Line Strike' + colorText.END
+            else:
+                dict['Pattern'] = colorText.BOLD + colorText.FAIL + '3 Line Strike' + colorText.END
+            saveDict['Pattern'] = '3 Line Strike'
+            return True
+        
+        check = talib.CDL3BLACKCROWS(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check.tail(1).item() != 0):
+            dict['Pattern'] = colorText.BOLD + colorText.FAIL + '3 Black Crows' + colorText.END
+            saveDict['Pattern'] = '3 Black Crows'
             return True
 
         check = talib.CDL3OUTSIDE(data['Open'], data['High'], data['Low'], data['Close'])
@@ -68,26 +61,34 @@ class CandlePatterns:
             dict['Pattern'] = colorText.BOLD + colorText.GREEN + '3 White Soldiers' + colorText.END
             saveDict['Pattern'] = '3 White Soldiers'
             return True
+
+        check = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check.tail(1).item() != 0):
+            if(check.tail(1).item() > 0):
+                dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Bullish Engulfing' + colorText.END
+                saveDict['Pattern'] = 'Bullish Engulfing'
+            else:
+                dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Bearish Engulfing' + colorText.END
+                saveDict['Pattern'] = 'Bearish Engulfing'
+            return True
+        
+        check = talib.CDLHAMMER(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check.tail(1).item() != 0):
+            dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Hammer' + colorText.END
+            saveDict['Pattern'] = 'Hammer'
+            return True
+
+        check = talib.CDLINVERTEDHAMMER(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check.tail(1).item() != 0):
+            dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Inverted Hammer' + colorText.END
+            saveDict['Pattern'] = 'Inverted Hammer'
+            return True
+
         
         check = talib.CDLDOJI(data['Open'], data['High'], data['Low'], data['Close'])
         if(check.tail(1).item() != 0):
             dict['Pattern'] = colorText.BOLD + 'Doji' + colorText.END
             saveDict['Pattern'] = 'Doji'
-            return True
-        
-        check = talib.CDL3LINESTRIKE(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
-                dict['Pattern'] = colorText.BOLD + colorText.GREEN + '3 Line Strike' + colorText.END
-            else:
-                dict['Pattern'] = colorText.BOLD + colorText.FAIL + '3 Line Strike' + colorText.END
-            saveDict['Pattern'] = '3 Line Strike'
-            return True
-        
-        check = talib.CDL3BLACKCROWS(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            dict['Pattern'] = colorText.BOLD + colorText.FAIL + '3 Black Crows' + colorText.END
-            saveDict['Pattern'] = '3 Black Crows'
             return True
         dict['Pattern'] = ''
         saveDict['Pattern'] = ''
