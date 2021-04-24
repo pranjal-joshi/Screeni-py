99725fb1b448772794faea3ad27d25625df77a7c
Pranjal Joshi <joshi.pranjal5@gmail.com>
2021-04-24 21:02:42 +0530

    v1.04 - OTA bug fixed
    v1.05 - Recently Screened results save/view with pickle
    
    v1.06 - Search by stock code added
    
    v1.07 - Script termination stopped by looping
    
    Code Refactored and GitHub Action Workflows added
    
    Documentation & badges added
    
    v1.08 - Prompt for saving excel file added

--- src/classes/OtaUpdater.py
+++ src/classes/OtaUpdater.py
@@ -5,7 +5,7 @@
  *  Description         :   Class for handling OTA updates
 '''
 
-from ColorText import colorText
+from classes.ColorText import colorText
 import requests
 import os
 import platform
@@ -39,19 +39,19 @@ sleep 3
 echo "[+] Screenipy Software Updater!"
 echo "[+] Downloading Software Update..."
 echo "[+] This may take some time as per your Internet Speed, Please Wait..."
-wget -q """ + url + """
+wget -q """ + url + """ -O screenipy.bin
 echo "[+] Update Completed!"
 rm updater.sh
         """
         f = open("updater.sh",'w')
         f.write(bashFile)
         f.close()
-        subprocess.Popen('bash updater.bat', shell=True)
+        subprocess.Popen('bash updater.sh', shell=True)
         sys.exit(0)
 
     # Check for update and download if available
     def checkForUpdate(proxyServer, VERSION="1.0"):
-        url = None
+        OTAUpdater.checkForUpdate.url = None
         try:
             resp = None
             now = float(VERSION)
@@ -59,35 +59,19 @@ rm updater.sh
                 resp = requests.get("https://api.github.com/repos/pranjal-joshi/Screeni-py/releases/latest",proxies={'https':proxyServer})
             else:
                 resp = requests.get("https://api.github.com/repos/pranjal-joshi/Screeni-py/releases/latest")
-            url = resp.json()['assets'][1]['browser_download_url']
+            OTAUpdater.checkForUpdate.url = resp.json()['assets'][1]['browser_download_url']
             size = int(resp.json()['assets'][1]['size']/(1024*1024))
             if platform.system() != 'Windows':
-                url = resp.json()['assets'][0]['browser_download_url']
+                OTAUpdater.checkForUpdate.url = resp.json()['assets'][0]['browser_download_url']
                 size = int(resp.json()['assets'][0]['size']/(1024*1024))
             if(float(resp.json()['tag_name']) > now):
                 action = str(input(colorText.BOLD + colorText.WARN + ('\n[+] New Software update (v%s) available. Download Now (Size: %dMB)? [Y/N]: ' % (str(resp.json()['tag_name']),size)))).lower()
                 if(action == 'y'):
                     try:
-                        '''
-                        print(colorText.BOLD + colorText.WARN + ('Downloading Update of %dMBs, This may take a few minutes, Please Wait...' % size) + colorText.END)
-                        download = requests.get(url, proxies={'https':proxyServer})
-                        fn = 'screenipy.exe'
-                        if platform.system() != 'Windows':
-                            fn = 'screenipy.bin'
-                        if os.path.exists(fn):
-                            os.remove(fn)
-                        with open(fn,'wb') as f:
-                            f.write(download.content)
-                        print(colorText.BOLD + colorText.GREEN + '[+] Update Completed.\n[+] Restart the program now.' + colorText.END)
-                        if platform.system() != 'Windows':
-                            os.system('chmod +x screenipy.bin')
-                        input('')
-                        sys.exit(0)
-                        '''
                         if platform.system() == 'Windows':
-                            OTAUpdater.updateForWindows(url)
+                            OTAUpdater.updateForWindows(OTAUpdater.checkForUpdate.url)
                         else:
-                            OTAUpdater.updateForLinux(url)
+                            OTAUpdater.updateForLinux(OTAUpdater.checkForUpdate.url)
                     except Exception as e:
                         print(colorText.BOLD + colorText.WARN + '[+] Error occured while updating!' + colorText.END)
                         raise(e)
@@ -98,5 +82,5 @@ rm updater.sh
         except Exception as e:
             print(colorText.BOLD + colorText.FAIL + "[+] Failure while checking update!" + colorText.END)
             print(e)
-            if url != None:
-                print(colorText.BOLD + colorText.BLUE + ("[+] Download update manually from %s\n" % url) + colorText.END)
+            if OTAUpdater.checkForUpdate.url != None:
+                print(colorText.BOLD + colorText.BLUE + ("[+] Download update manually from %s\n" % OTAUpdater.checkForUpdate.url) + colorText.END)
---
+++ src/release.md
@@ -0,0 +1,23 @@
+# Screeni-py
+### A Python-based stock screener for NSE, India.
+
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/pranjal-joshi/Screeni-py)
+## What's New?
+1. Program Window closing issue fixed.
+2. `Option > 0` added - Screen stocks by entering it's name (stock code).
+3. Stability fixes and improvements.
+4. Last screened results will be stored and can be viewed with `Option > 7`.
+5. Now get the Software Updates for every new feature that we release! [OTA software update delivery has been fixed.]
+
+## Downloads
+* For **Windows** users, download **[screenipy.exe](https://github.com/pranjal-joshi/Screeni-py/releases/download/1.07/screenipy.exe)**
+* For **Linux** users, download **[screenipy.bin](https://github.com/pranjal-joshi/Screeni-py/releases/download/1.07/screenipy.bin)**
+
+## How to use?
+
+[**Click Here**](https://github.com/pranjal-joshi/Screeni-py) to read the documentation.
+
+## Disclaimer:
+* DO NOT use the result provided by the software 'solely' to make your trading decisions.
+* Always backtest and analyze the stocks manually before you trade.
+* The Author(s) and the software will not be held liable for your losses.
