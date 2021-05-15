cc07babc27ceb225b9d814a64bb973a7093f420b
swarpatel23 <smp1613@gmail.com>
2021-05-15 22:14:22 +0530

    running stock screener using multiprocessing to run in parallel

--- src/classes/Fetcher.py
+++ src/classes/Fetcher.py
@@ -21,10 +21,14 @@ screenCounter = 1
 nse = Nse()
 
 # Exception class if yfinance stock delisted
+
+
 class StockDataEmptyException(Exception):
     pass
 
 # This Class Handles Fetching of Stock Data over the internet
+
+
 class tools:
 
     # Fetch all stock codes from NSE
@@ -33,48 +37,60 @@ class tools:
         if executeOption == 0:
             stockCode = None
             while stockCode == None or stockCode == "":
-                stockCode = str(input(colorText.BOLD + colorText.BLUE + "[+] Enter Stock Code(s) for screening (Multiple codes should be seperated by ,): ")).upper()    
-            stockCode = stockCode.replace(" ","")
+                stockCode = str(input(colorText.BOLD + colorText.BLUE +
+                                      "[+] Enter Stock Code(s) for screening (Multiple codes should be seperated by ,): ")).upper()
+            stockCode = stockCode.replace(" ", "")
             listStockCodes = stockCode.split(',')
         else:
-            print(colorText.BOLD + "[+] Getting Stock Codes From NSE... ", end='')
+            print(colorText.BOLD +
+                  "[+] Getting Stock Codes From NSE... ", end='')
             listStockCodes = list(nse.get_stock_codes(cached=False))[1:]
             if len(listStockCodes) > 10:
-                print(colorText.GREEN + ("=> Done! Fetched %d stock codes." % len(listStockCodes)) + colorText.END)
+                print(colorText.GREEN + ("=> Done! Fetched %d stock codes." %
+                                         len(listStockCodes)) + colorText.END)
                 if ConfigManager.shuffleEnabled:
                     random.shuffle(listStockCodes)
-                    print(colorText.BLUE + "[+] Stock shuffling is active." + colorText.END)
+                    print(colorText.BLUE +
+                          "[+] Stock shuffling is active." + colorText.END)
                 else:
-                    print(colorText.FAIL + "[+] Stock shuffling is inactive." + colorText.END)
+                    print(colorText.FAIL +
+                          "[+] Stock shuffling is inactive." + colorText.END)
                 if ConfigManager.stageTwo:
-                    print(colorText.BLUE + "[+] Screening only for the stocks in Stage-2! Edit User Config to change this." + colorText.END)
+                    print(
+                        colorText.BLUE + "[+] Screening only for the stocks in Stage-2! Edit User Config to change this." + colorText.END)
                 else:
-                    print(colorText.FAIL + "[+] Screening only for the stocks in all Stages! Edit User Config to change this." + colorText.END)
-                
+                    print(
+                        colorText.FAIL + "[+] Screening only for the stocks in all Stages! Edit User Config to change this." + colorText.END)
+
             else:
-                input(colorText.FAIL + "=> Error getting stock codes from NSE! Press any key to exit!" + colorText.END)
+                input(
+                    colorText.FAIL + "=> Error getting stock codes from NSE! Press any key to exit!" + colorText.END)
                 sys.exit("Exiting script..")
 
     # Fetch stock price data from Yahoo finance
     def fetchStockData(stockCode, period, duration, proxyServer, screenResults):
         global screenCounter
-        with SuppressOutput(suppress_stdout = True, suppress_stderr = True):
+        with SuppressOutput(suppress_stdout=True, suppress_stderr=True):
             data = yf.download(
-                tickers = stockCode+".NS",
-                period = period,
-                duration = duration,
-                proxy = proxyServer,
+                tickers=stockCode+".NS",
+                period=period,
+                duration=duration,
+                proxy=proxyServer,
                 progress=False
             )
         sys.stdout.write("\r\033[K")
         try:
-            print(colorText.BOLD + colorText.GREEN + ("[%d%%] Screened %d, Found %d. Fetching data & Analyzing %s..." % (int(screenCounter/len(listStockCodes)*100), screenCounter, len(screenResults), stockCode)) + colorText.END, end='')
+            # print(colorText.BOLD + colorText.GREEN + ("[%d%%] Screened %d, Found %d. Fetching data & Analyzing %s..." % (int(screenCounter/len(listStockCodes)*100), screenCounter, len(screenResults), stockCode)) + colorText.END, end='')
+            print(colorText.BOLD + colorText.GREEN +
+                  ("Fetching data & Analyzing %s..." % (stockCode)) + colorText.END, end='')
         except ZeroDivisionError:
             pass
         if len(data) == 0:
-            print(colorText.BOLD + colorText.FAIL + "=> Failed to fetch!" + colorText.END, end='\r', flush=True)
+            print(colorText.BOLD + colorText.FAIL +
+                  "=> Failed to fetch!" + colorText.END, end='\r', flush=True)
             raise StockDataEmptyException
             return None
-        print(colorText.BOLD + colorText.GREEN + "=> Done!" + colorText.END, end='\r', flush=True)
+        print(colorText.BOLD + colorText.GREEN + "=> Done!" +
+              colorText.END, end='\r', flush=True)
         screenCounter += 1
         return data
--- src/screenipy.py
+++ src/screenipy.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 from tabulate import tabulate
 from time import sleep
+import multiprocessing
 import platform
 import datetime
 import classes.Fetcher as Fetcher
@@ -27,36 +28,11 @@ from classes.Changelog import *
 TEST_STKCODE = "SBIN"
 
 # Constants
-daysForLowestVolume = 30
 np.seterr(divide='ignore', invalid='ignore')
 
 # Global Variabls
 candlePatterns = CandlePatterns()
-screenResults = pd.DataFrame(columns=['Stock','Consolidating','Breaking-Out','MA-Signal','Volume','LTP','RSI','Trend','Pattern'])
-saveResults = pd.DataFrame(columns=['Stock','Consolidating','Breaking-Out','MA-Signal','Volume','LTP','RSI','Trend','Pattern'])
-screeningDictionary = {
-    'Stock': "",
-    'Consolidating': "",
-    'Breaking-Out': "",
-    'MA-Signal': "",
-    'Volume': "",
-    'LTP': 0,
-    'RSI': 0,
-    'Trend': "",
-    'Pattern': ""
-}
-saveDictionary = {
-    'Stock': "",
-    'Pattern': "",
-    'Consolidating': "",
-    'Breaking-Out': "",
-    'MA-Signal': "",
-    'Volume': "",
-    'LTP': 0,
-    'RSI': 0,
-    'Trend': "",
-    'Pattern': ""
-}
+
 
 # Get system wide proxy for networking
 try:
@@ -65,8 +41,11 @@ except KeyError:
     proxyServer = ""
 
 # Manage Execution flow
+
+
 def initExecution():
-    print(colorText.BOLD + colorText.WARN + '[+] Press a number to start stock screening: ' + colorText.END)
+    print(colorText.BOLD + colorText.WARN +
+          '[+] Press a number to start stock screening: ' + colorText.END)
     print(colorText.BOLD + '''     0 > Screen stocks by stock name (NSE Stock Code)
      1 > Screen stocks for Breakout or Consolidation
      2 > Screen for the stocks with recent Breakout & Volume
@@ -80,7 +59,7 @@ def initExecution():
     10 > Show Last Screened Results
     11 > About Developer
     12 > Exit''' + colorText.END
-    )
+          )
     result = input(colorText.BOLD + colorText.FAIL + '[+] Select option: ')
     print(colorText.END, end='')
     try:
@@ -89,35 +68,117 @@ def initExecution():
             raise ValueError
         return result
     except:
-        print(colorText.BOLD + colorText.FAIL + '\n[+] Please enter a valid numeric option & Try Again!' + colorText.END)
+        print(colorText.BOLD + colorText.FAIL +
+              '\n[+] Please enter a valid numeric option & Try Again!' + colorText.END)
         sleep(2)
         Utility.tools.clearScreen()
         return initExecution()
 
+
+def screenStocks(executeOption, reversalOption, daysForLowestVolume, minRSI, maxRSI, respBullBear, insideBarToLookback, stock):
+    screenResults = pd.DataFrame(columns=[
+        'Stock', 'Consolidating', 'Breaking-Out', 'MA-Signal', 'Volume', 'LTP', 'RSI', 'Trend', 'Pattern'])
+    screeningDictionary = {'Stock': "", 'Consolidating': "",  'Breaking-Out': "",
+                           'MA-Signal': "", 'Volume': "", 'LTP': 0, 'RSI': 0, 'Trend': "", 'Pattern': ""}
+    saveDictionary = {'Stock': "", 'Pattern': "", 'Consolidating': "", 'Breaking-Out': "",
+                      'MA-Signal': "", 'Volume': "", 'LTP': 0, 'RSI': 0, 'Trend': "", 'Pattern': ""}
+
+    try:
+        data = Fetcher.tools.fetchStockData(stock,
+                                            ConfigManager.period,
+                                            ConfigManager.duration,
+                                            proxyServer,
+                                            screenResults)
+        fullData, processedData = Screener.tools.preprocessData(
+            data, daysToLookback=ConfigManager.daysToLookback)
+        if not processedData.empty:
+            screeningDictionary['Stock'] = colorText.BOLD + \
+                colorText.BLUE + stock + colorText.END
+            saveDictionary['Stock'] = stock
+            consolidationValue = Screener.tools.validateConsolidation(
+                processedData, screeningDictionary, saveDictionary, percentage=ConfigManager.consolidationPercentage)
+            isMaReversal = Screener.tools.validateMovingAverages(
+                processedData, screeningDictionary, saveDictionary, range=1.25)
+            isVolumeHigh = Screener.tools.validateVolume(
+                processedData, screeningDictionary, saveDictionary, volumeRatio=ConfigManager.volumeRatio)
+            isBreaking = Screener.tools.findBreakout(
+                processedData, screeningDictionary, saveDictionary, daysToLookback=ConfigManager.daysToLookback)
+            isLtpValid = Screener.tools.validateLTP(
+                fullData, screeningDictionary, saveDictionary, minLTP=ConfigManager.minLTP, maxLTP=ConfigManager.maxLTP)
+            isLowestVolume = Screener.tools.validateLowestVolume(
+                processedData, daysForLowestVolume)
+            isValidRsi = Screener.tools.validateRSI(
+                processedData, screeningDictionary, saveDictionary, minRSI, maxRSI)
+            currentTrend = Screener.tools.findTrend(
+                processedData, screeningDictionary, saveDictionary, daysToLookback=ConfigManager.daysToLookback, stockName=stock)
+            isCandlePattern = candlePatterns.findPattern(
+                processedData, screeningDictionary, saveDictionary)
+            isInsideBar = Screener.tools.validateInsideBar(
+                processedData, screeningDictionary, saveDictionary, bullBear=respBullBear, daysToLookback=insideBarToLookback)
+
+            if executeOption == 0:
+                return screeningDictionary, saveDictionary
+            if (executeOption == 1 or executeOption == 2) and isBreaking and isVolumeHigh and isLtpValid:
+                return screeningDictionary, saveDictionary
+            if (executeOption == 1 or executeOption == 3) and (consolidationValue <= ConfigManager.consolidationPercentage and consolidationValue != 0) and isLtpValid:
+                return screeningDictionary, saveDictionary
+            if executeOption == 4 and isLtpValid and isLowestVolume:
+                return screeningDictionary, saveDictionary
+            if executeOption == 5 and isLtpValid and isValidRsi:
+                return screeningDictionary, saveDictionary
+            if executeOption == 6 and isLtpValid:
+                if reversalOption == 1:
+                    if saveDictionary['Pattern'] in CandlePatterns.reversalPatternsBullish or isMaReversal > 0:
+                        return screeningDictionary, saveDictionary
+                elif reversalOption == 2:
+                    if saveDictionary['Pattern'] in CandlePatterns.reversalPatternsBearish or isMaReversal < 0:
+                        return screeningDictionary, saveDictionary
+            if executeOption == 7 and isLtpValid and isInsideBar:
+                return screeningDictionary, saveDictionary
+    except KeyboardInterrupt:
+        print(colorText.BOLD + colorText.FAIL +
+              "\n[+] Script terminated by the user." + colorText.END)
+    except Fetcher.StockDataEmptyException:
+        pass
+    except Exception as e:
+        print(colorText.FAIL +
+              ("\n[+] Exception Occured while Screening %s! Skipping this stock.." % stock) + colorText.END)
+    return
+
 # Main function
+
+
 def main(testing=False):
-    global daysForLowestVolume, screenResults, saveResults
+
+    screenResults = pd.DataFrame(columns=[
+        'Stock', 'Consolidating', 'Breaking-Out', 'MA-Signal', 'Volume', 'LTP', 'RSI', 'Trend', 'Pattern'])
+    saveResults = pd.DataFrame(columns=['Stock', 'Consolidating', 'Breaking-Out',
+                                        'MA-Signal', 'Volume', 'LTP', 'RSI', 'Trend', 'Pattern'])
     Fetcher.screenCounter = 1
     minRSI = 0
     maxRSI = 100
     insideBarToLookback = 7
     respBullBear = 1
-    screenResults = pd.DataFrame(columns=['Stock','Consolidating','Breaking-Out','MA-Signal','Volume','LTP','RSI','Trend','Pattern'])
-    saveResults = pd.DataFrame(columns=['Stock','Consolidating','Breaking-Out','MA-Signal','Volume','LTP','RSI','Trend','Pattern'])
+    daysForLowestVolume = 30
+    reversalOption = None
+
     executeOption = initExecution()
     if executeOption == 4:
         try:
-            daysForLowestVolume = int(input(colorText.BOLD + colorText.WARN + '\n[+] The Volume should be lowest since last how many candles? '))
+            daysForLowestVolume = int(input(colorText.BOLD + colorText.WARN +
+                                            '\n[+] The Volume should be lowest since last how many candles? '))
         except ValueError:
             print(colorText.END)
-            print(colorText.BOLD + colorText.FAIL + '[+] Error: Non-numeric value entered! Screening aborted.' + colorText.END)
+            print(colorText.BOLD + colorText.FAIL +
+                  '[+] Error: Non-numeric value entered! Screening aborted.' + colorText.END)
             input('')
             main()
         print(colorText.END)
     if executeOption == 5:
         minRSI, maxRSI = Utility.tools.promptRSIValues()
         if (not minRSI and not maxRSI):
-            print(colorText.BOLD + colorText.FAIL + '\n[+] Error: Invalid values for RSI! Values should be in range of 0 to 100. Screening aborted.' + colorText.END)
+            print(colorText.BOLD + colorText.FAIL +
+                  '\n[+] Error: Invalid values for RSI! Values should be in range of 0 to 100. Screening aborted.' + colorText.END)
             input('')
             main()
     if executeOption == 6:
@@ -141,102 +202,58 @@ def main(testing=False):
         Utility.tools.showDevInfo()
         main()
     if executeOption == 12:
-        print(colorText.BOLD + colorText.FAIL + "[+] Script terminated by the user." + colorText.END)
+        print(colorText.BOLD + colorText.FAIL +
+              "[+] Script terminated by the user." + colorText.END)
         sys.exit(0)
     if executeOption >= 0 and executeOption < 8:
         ConfigManager.tools.getConfig(ConfigManager.parser)
         try:
             Fetcher.tools.fetchStockCodes(executeOption)
         except urllib.error.URLError:
-            print(colorText.BOLD + colorText.FAIL + "\n\n[+] Oops! It looks like you don't have an Internet connectivity at the moment! Press any key to exit!" + colorText.END)
+            print(colorText.BOLD + colorText.FAIL +
+                  "\n\n[+] Oops! It looks like you don't have an Internet connectivity at the moment! Press any key to exit!" + colorText.END)
             input('')
             sys.exit(0)
-        print(colorText.BOLD + colorText.WARN + "[+] Starting Stock Screening.. Press Ctrl+C to stop!\n")
-        for stock in Fetcher.listStockCodes:
-            try:
-                data = Fetcher.tools.fetchStockData(stock, 
-                            ConfigManager.period,
-                            ConfigManager.duration,
-                            proxyServer,
-                            screenResults
-                        )
-                fullData, processedData = Screener.tools.preprocessData(data, daysToLookback=ConfigManager.daysToLookback)
-                if not processedData.empty:
-                    screeningDictionary['Stock'] = colorText.BOLD + colorText.BLUE + stock + colorText.END
-                    saveDictionary['Stock'] = stock
-                    consolidationValue = Screener.tools.validateConsolidation(processedData, screeningDictionary, saveDictionary, percentage=ConfigManager.consolidationPercentage)
-                    isMaReversal = Screener.tools.validateMovingAverages(processedData, screeningDictionary, saveDictionary, range=1.25)
-                    isVolumeHigh = Screener.tools.validateVolume(processedData, screeningDictionary, saveDictionary, volumeRatio=ConfigManager.volumeRatio)
-                    isBreaking = Screener.tools.findBreakout(processedData, screeningDictionary, saveDictionary, daysToLookback=ConfigManager.daysToLookback)
-                    isLtpValid = Screener.tools.validateLTP(fullData, screeningDictionary, saveDictionary, minLTP=ConfigManager.minLTP, maxLTP=ConfigManager.maxLTP)
-                    isLowestVolume = Screener.tools.validateLowestVolume(processedData, daysForLowestVolume)
-                    isValidRsi = Screener.tools.validateRSI(processedData, screeningDictionary, saveDictionary, minRSI, maxRSI)
-                    currentTrend = Screener.tools.findTrend(processedData, screeningDictionary, saveDictionary, daysToLookback=ConfigManager.daysToLookback, stockName=stock)
-                    isCandlePattern = candlePatterns.findPattern(processedData, screeningDictionary, saveDictionary)
-                    isInsideBar = Screener.tools.validateInsideBar(processedData, screeningDictionary, saveDictionary, bullBear=respBullBear, daysToLookback=insideBarToLookback)
-                    if executeOption == 0:
-                        screenResults = screenResults.append(screeningDictionary,ignore_index=True)
-                        saveResults = saveResults.append(saveDictionary, ignore_index=True)
-                    if (executeOption == 1 or executeOption == 2) and isBreaking and isVolumeHigh and isLtpValid:
-                        screenResults = screenResults.append(screeningDictionary,ignore_index=True)
-                        saveResults = saveResults.append(saveDictionary, ignore_index=True)
-                    if (executeOption == 1 or executeOption == 3) and (consolidationValue <= ConfigManager.consolidationPercentage and consolidationValue != 0) and isLtpValid:
-                        screenResults = screenResults.append(screeningDictionary,ignore_index=True)
-                        saveResults = saveResults.append(saveDictionary, ignore_index=True)
-                    if executeOption == 4 and isLtpValid and isLowestVolume:
-                        screenResults = screenResults.append(screeningDictionary,ignore_index=True)
-                        saveResults = saveResults.append(saveDictionary, ignore_index=True)
-                    if executeOption == 5 and isLtpValid and isValidRsi:
-                        screenResults = screenResults.append(screeningDictionary,ignore_index=True)
-                        saveResults = saveResults.append(saveDictionary, ignore_index=True)
-                    if executeOption == 6 and isLtpValid:
-                        if reversalOption == 1:
-                            if saveDictionary['Pattern'] in CandlePatterns.reversalPatternsBullish or isMaReversal > 0:
-                                screenResults = screenResults.append(screeningDictionary,ignore_index=True)
-                                saveResults = saveResults.append(saveDictionary, ignore_index=True)
-                        elif reversalOption == 2:
-                            if saveDictionary['Pattern'] in CandlePatterns.reversalPatternsBearish or isMaReversal < 0:
-                                screenResults = screenResults.append(screeningDictionary,ignore_index=True)
-                                saveResults = saveResults.append(saveDictionary, ignore_index=True)
-                    if executeOption == 7 and isLtpValid and isInsideBar:
-                        screenResults = screenResults.append(screeningDictionary,ignore_index=True)
-                        saveResults = saveResults.append(saveDictionary, ignore_index=True)
-                if testing and len(screenResults):
-                    break
-            except KeyboardInterrupt:
-                print(colorText.BOLD + colorText.FAIL + "\n[+] Script terminated by the user." + colorText.END)
-                break
-            except Fetcher.StockDataEmptyException:
-                pass
-            except Exception as e:
-                # print(colorText.FAIL + ("\n[+] Exception Occured while Screening %s! Skipping this stock.." % stock) + colorText.END)
-                if testing:
-                    raise e
+        print(colorText.BOLD + colorText.WARN +
+              "[+] Starting Stock Screening.. Press Ctrl+C to stop!\n")
+
+        items = [(executeOption, reversalOption, daysForLowestVolume, minRSI, maxRSI, respBullBear, insideBarToLookback, stock)
+                 for stock in Fetcher.listStockCodes]
+        results = pool.starmap(screenStocks, items)
+        results = list(filter(None, results))
+        for x, y in results:
+            screenResults = screenResults.append(x, ignore_index=True)
+            saveResults = saveResults.append(y, ignore_index=True)
+
         screenResults.sort_values(by=['Stock'], ascending=True, inplace=True)
         saveResults.sort_values(by=['Stock'], ascending=True, inplace=True)
         screenResults.rename(
             columns={
-                'Trend':f'Trend ({ConfigManager.daysToLookback}Days)',
-                'Breaking-Out':'Breakout-Levels'
-                },
+                'Trend': f'Trend ({ConfigManager.daysToLookback}Days)',
+                'Breaking-Out': 'Breakout-Levels'
+            },
             inplace=True
         )
         saveResults.rename(
             columns={
-                'Trend':f'Trend ({ConfigManager.daysToLookback}Days)',
-                'Breaking-Out':'Breakout-Levels'
-                },
+                'Trend': f'Trend ({ConfigManager.daysToLookback}Days)',
+                'Breaking-Out': 'Breakout-Levels'
+            },
             inplace=True
         )
         print(tabulate(screenResults, headers='keys', tablefmt='psql'))
         Utility.tools.setLastScreenedResults(screenResults)
         Utility.tools.promptSaveResults(saveResults)
-        print(colorText.BOLD + colorText.WARN + "[+] Note: Trend calculation is based on number of days recent to screen as per your configuration." + colorText.END)
-        print(colorText.BOLD + colorText.GREEN + "[+] Screening Completed! Happy Trading! :)" + colorText.END)
+        print(colorText.BOLD + colorText.WARN +
+              "[+] Note: Trend calculation is based on number of days recent to screen as per your configuration." + colorText.END)
+        print(colorText.BOLD + colorText.GREEN +
+              "[+] Screening Completed! Happy Trading! :)" + colorText.END)
         input('')
         main()
+
 
 if __name__ == "__main__":
     Utility.tools.clearScreen()
     OTAUpdater.checkForUpdate(proxyServer, VERSION)
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
     main()
