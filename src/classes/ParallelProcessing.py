622894040cfe030507aa967dd49d48dd670e9f25
swarpatel23 <smp1613@gmail.com>
2021-05-23 10:28:58 +0530

    checking tradingTime only once at begining of workers

--- src/classes/ParallelProcessing.py
+++ src/classes/ParallelProcessing.py
@@ -39,6 +39,8 @@ class StockConsumer(multiprocessing.Process):
         self.curr = datetime.now(pytz.timezone('Asia/Kolkata'))
         self.openTime = self.curr.replace(hour=9, minute=15)
         self.closeTime = self.curr.replace(hour=15, minute=30)
+        self.isTradingTime = ((self.openTime <= self.curr <= self.closeTime) and (
+            0 <= self.curr.weekday() <= 4))
 
     def run(self):
         # while True:
@@ -54,10 +56,6 @@ class StockConsumer(multiprocessing.Process):
             self.task_queue.task_done()
             self.result_queue.put(answer)
 
-    def isTradingTime(self):
-        currTime = datetime.now(pytz.timezone('Asia/Kolkata'))
-        return (self.openTime <= currTime <= self.closeTime) and (0 <= currTime.weekday() <= 4)
-
     def screenStocks(self, executeOption, reversalOption, daysForLowestVolume, minRSI, maxRSI, respBullBear, insideBarToLookback, totalSymbols,
                      configManager, fetcher, screener, candlePatterns, stock):
         screenResults = pd.DataFrame(columns=[
@@ -68,7 +66,7 @@ class StockConsumer(multiprocessing.Process):
                           'MA-Signal': "", 'Volume': "", 'LTP': 0, 'RSI': 0, 'Trend': "", 'Pattern': ""}
 
         try:
-            if self.isTradingTime() or (self.stockDict.get(stock) is None) or (configManager.cacheEnabled == False):
+            if (self.stockDict.get(stock) is None) or (configManager.cacheEnabled == False) or self.isTradingTime:
                 data = fetcher.fetchStockData(stock,
                                               configManager.period,
                                               configManager.duration,
@@ -76,7 +74,7 @@ class StockConsumer(multiprocessing.Process):
                                               self.screenResultsCounter,
                                               self.screenCounter,
                                               totalSymbols)
-                if configManager.cacheEnabled == True and not self.isTradingTime():
+                if configManager.cacheEnabled == True and not self.isTradingTime:
                     self.stockDict[stock] = data.to_dict('split')
             else:
                 try:
