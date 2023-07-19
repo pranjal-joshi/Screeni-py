4429fa0715ef53cbccfbbf5f426ba4953dd75f5b
pranjal-joshi <joshi.pranjal5@gmail.com>
2023-07-19 09:44:12 +0530

    wip -  to debug error after wrapper replacement

--- src/classes/CandlePatterns.py
+++ src/classes/CandlePatterns.py
@@ -6,7 +6,8 @@
 '''
 
 import pandas as pd
-import talib
+# import talib
+from classes.ScreenipyTA import ScreenerTA
 from classes.ColorText import colorText
 
 class CandlePatterns:
@@ -23,33 +24,33 @@ class CandlePatterns:
         data = data.head(4)
         data = data[::-1]
 
-        check = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Morning Star' + colorText.END
             saveDict['Pattern'] = 'Morning Star'
             return True
 
-        check = talib.CDLMORNINGDOJISTAR(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLMORNINGDOJISTAR(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Morning Doji Star' + colorText.END
             saveDict['Pattern'] = 'Morning Doji Star'
             return True
         
-        check = talib.CDLEVENINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLEVENINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Evening Star' + colorText.END
             saveDict['Pattern'] = 'Evening Star'
             return True
 
-        check = talib.CDLEVENINGDOJISTAR(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLEVENINGDOJISTAR(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Evening Doji Star' + colorText.END
             saveDict['Pattern'] = 'Evening Doji Star'
             return True
 
-        check = talib.CDLLADDERBOTTOM(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
+        check = ScreenerTA.CDLLADDERBOTTOM(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
+            if(check is not None and check.tail(1).item() > 0):
                 dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Ladder Bottom' + colorText.END
                 saveDict['Pattern'] = 'Bullish Ladder Bottom'
             else:
@@ -57,24 +58,24 @@ class CandlePatterns:
                 saveDict['Pattern'] = 'Bearish Ladder Bottom'
             return True
 
-        check = talib.CDL3LINESTRIKE(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
+        check = ScreenerTA.CDL3LINESTRIKE(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
+            if(check is not None and check.tail(1).item() > 0):
                 dict['Pattern'] = colorText.BOLD + colorText.GREEN + '3 Line Strike' + colorText.END
             else:
                 dict['Pattern'] = colorText.BOLD + colorText.FAIL + '3 Line Strike' + colorText.END
             saveDict['Pattern'] = '3 Line Strike'
             return True
         
-        check = talib.CDL3BLACKCROWS(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDL3BLACKCROWS(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.FAIL + '3 Black Crows' + colorText.END
             saveDict['Pattern'] = '3 Black Crows'
             return True
 
-        check = talib.CDL3INSIDE(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
+        check = ScreenerTA.CDL3INSIDE(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
+            if(check is not None and check.tail(1).item() > 0):
                 dict['Pattern'] = colorText.BOLD + colorText.GREEN + '3 Outside Up' + colorText.END
                 saveDict['Pattern'] = '3 Inside Up'
             else:
@@ -82,9 +83,9 @@ class CandlePatterns:
                 saveDict['Pattern'] = '3 Inside Down'
             return True
 
-        check = talib.CDL3OUTSIDE(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
+        check = ScreenerTA.CDL3OUTSIDE(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
+            if(check is not None and check.tail(1).item() > 0):
                 dict['Pattern'] = colorText.BOLD + colorText.GREEN + '3 Outside Up' + colorText.END
                 saveDict['Pattern'] = '3 Outside Up'
             else:
@@ -92,15 +93,15 @@ class CandlePatterns:
                 saveDict['Pattern'] = '3 Outside Down'
             return True
 
-        check = talib.CDL3WHITESOLDIERS(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDL3WHITESOLDIERS(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.GREEN + '3 White Soldiers' + colorText.END
             saveDict['Pattern'] = '3 White Soldiers'
             return True
 
-        check = talib.CDLHARAMI(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
+        check = ScreenerTA.CDLHARAMI(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
+            if(check is not None and check.tail(1).item() > 0):
                 dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Bullish Harami' + colorText.END
                 saveDict['Pattern'] = 'Bullish Harami'
             else:
@@ -108,9 +109,9 @@ class CandlePatterns:
                 saveDict['Pattern'] = 'Bearish Harami'
             return True
 
-        check = talib.CDLHARAMICROSS(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
+        check = ScreenerTA.CDLHARAMICROSS(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
+            if(check is not None and check.tail(1).item() > 0):
                 dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Bullish Harami Cross' + colorText.END
                 saveDict['Pattern'] = 'Bullish Harami Cross'
             else:
@@ -118,9 +119,9 @@ class CandlePatterns:
                 saveDict['Pattern'] = 'Bearish Harami Cross'
             return True
 
-        check = talib.CDLMARUBOZU(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
+        check = ScreenerTA.CDLMARUBOZU(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
+            if(check is not None and check.tail(1).item() > 0):
                 dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Bullish Marubozu' + colorText.END
                 saveDict['Pattern'] = 'Bullish Marubozu'
             else:
@@ -128,51 +129,51 @@ class CandlePatterns:
                 saveDict['Pattern'] = 'Bearish Marubozu'
             return True
 
-        check = talib.CDLHANGINGMAN(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLHANGINGMAN(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Hanging Man' + colorText.END
             saveDict['Pattern'] = 'Hanging Man'
             return True
         
-        check = talib.CDLHAMMER(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLHAMMER(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Hammer' + colorText.END
             saveDict['Pattern'] = 'Hammer'
             return True
 
-        check = talib.CDLINVERTEDHAMMER(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLINVERTEDHAMMER(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Inverted Hammer' + colorText.END
             saveDict['Pattern'] = 'Inverted Hammer'
             return True
 
-        check = talib.CDLSHOOTINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLSHOOTINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Shooting Star' + colorText.END
             saveDict['Pattern'] = 'Shooting Star'
             return True
 
-        check = talib.CDLDRAGONFLYDOJI(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLDRAGONFLYDOJI(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Dragonfly Doji' + colorText.END
             saveDict['Pattern'] = 'Dragonfly Doji'
             return True
 
-        check = talib.CDLGRAVESTONEDOJI(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLGRAVESTONEDOJI(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + colorText.FAIL + 'Gravestone Doji' + colorText.END
             saveDict['Pattern'] = 'Gravestone Doji'
             return True
 
-        check = talib.CDLDOJI(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
+        check = ScreenerTA.CDLDOJI(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
             dict['Pattern'] = colorText.BOLD + 'Doji' + colorText.END
             saveDict['Pattern'] = 'Doji'
             return True
 
-        check = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
-        if(check.tail(1).item() != 0):
-            if(check.tail(1).item() > 0):
+        check = ScreenerTA.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
+        if(check):
+            if(check is not None and check.tail(1).item() > 0):
                 dict['Pattern'] = colorText.BOLD + colorText.GREEN + 'Bullish Engulfing' + colorText.END
                 saveDict['Pattern'] = 'Bullish Engulfing'
             else:
