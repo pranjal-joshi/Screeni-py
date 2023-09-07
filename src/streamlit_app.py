18d0ee8f15db8f3ee08efc490435f2db5e0d1578
pranjal-joshi <joshi.pranjal5@gmail.com>
2023-09-07 17:21:08 +0530

    wip - ta-lib installation removed

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -8,47 +8,6 @@ st.set_page_config(layout="wide")
 
 # Set protobuf to python to avoid TF error (This is a Slower infernece)
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
-os.environ["TERM"] = "xterm"
-
-# check if the library folder already exists, to avoid building everytime you load the pahe
-if not os.path.isdir("/tmp/ta-lib"):
-
-    st.title("Please wait while we're Unfreezing the system 😉")
-    st.snow()
-
-    # Download ta-lib to disk
-    with open("/tmp/ta-lib-0.4.0-src.tar.gz", "wb") as file:
-        response = requests.get(
-            "http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
-        )
-        file.write(response.content)
-    # get our current dir, to configure it back again. Just house keeping
-    default_cwd = os.getcwd()
-    os.chdir("/tmp")
-    # untar
-    os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
-    os.chdir("/tmp/ta-lib")
-    os.system("ls -la /app/equity/")
-    # build
-    os.system("./configure --prefix=/home/appuser")
-    os.system("make")
-    # install
-    os.system("make install")
-    # back to the cwd
-    os.chdir(default_cwd)
-    sys.stdout.flush()
-
-# add the library to our current environment
-from ctypes import *
-
-lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
-# import library
-try:
-    import talib
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib==0.4.24", "numpy==1.21.0"])
-finally:
-    import talib
 
 
 import streamlit as st
@@ -60,6 +19,7 @@ execute_inputs = []
 
 def on_start_button_click():
     global execute_inputs
+    st.write(execute_inputs)
     my_bar = st.progress(0, text="Starting Stocks Screening...")
     with patch('builtins.input', side_effect=execute_inputs):
         try:
@@ -269,6 +229,8 @@ with st.container():
       stock_column = df.pop('Stock')  # Remove 'Age' column and store it separately
       df.insert(0, 'Stock', stock_column)
       st.write(df.to_html(escape=False, index=False, index_names=False), unsafe_allow_html=True)
+    except FileNotFoundError:
+      st.error('Last Screened results are not available at the moment')
     except Exception as e:
       st.error('No Dataframe found for last_screened_results.pkl')
       st.exception(e)
